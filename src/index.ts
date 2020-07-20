6bc56efaed65ed70913cf56e58cc17806b538754
Timofey Goncharov <gtosss@mail.ru>
2020-07-20 20:05:44 +0300

    Fix export for useError.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import useForm from './form';
 import useFieldArray from './field-array';
+import useError from './error';
 import {deleteIn, getIn, setIn, makeNested, removeFromInlineMap} from './utils/object-manager'
 
 export {
   useForm,
+  useError,
   useFieldArray,
   deleteIn,
   getIn,
