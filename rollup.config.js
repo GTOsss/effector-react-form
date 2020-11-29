ae8839be30023a96baf835165756c852525173c2
Timofey Goncharov <gtosss@mail.ru>
2020-11-29 19:50:33 +0300

    Add rollup build script. Refactor 'package.json'.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
   plugins: [
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
     babel({
       babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.ts'],
     }),
+    terser(),
   ],
-  external: ['lodash.topath', 'src/__test__'],
+  external: ['lodash.topath', 'react'],
   output: [
     {
       file: 'dist/bundle.cjs.js',
---
+++ tools/builder/rollup.js
@@ -0,0 +1,76 @@
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const { babel } = require('@rollup/plugin-babel');
+// const { terser } = require('rollup-plugin-terser');
+const rollup = require('rollup');
+
+const input = 'src/index.ts';
+const external = ['lodash.topath', 'react', 'effector', 'effector-react', 'effector-react/ssr'];
+const outputDirCSR = 'dist';
+const outputDirSSR = 'dist/ssr';
+
+configCSR = {
+  input,
+  plugins: [
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.ts'],
+    }),
+    terser(),
+  ],
+  external,
+};
+
+const outputCSR = [
+  {
+    file: `${outputDirCSR}/bundle.cjs.js`,
+    format: 'cjs',
+  },
+  {
+    file: `${outputDirCSR}/bundle.esm.js`,
+    format: 'esm',
+  },
+];
+
+const configSSR = {
+  input,
+  plugins: [
+    nodeResolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.ts'],
+      plugins: [['effector/babel-plugin', { reactSsr: true }]],
+    }),
+    terser(),
+  ],
+  external,
+};
+
+const outputSSR = [
+  {
+    file: `${outputDirSSR}/ssr/bundle.cjs.js`,
+    format: 'cjs',
+  },
+  {
+    file: `${outputDirSSR}/ssr/bundle.esm.js`,
+    format: 'esm',
+  },
+];
+
+const build = async () => {
+  // CSR
+  const bundleCSR = await rollup.rollup(configCSR);
+  await Promise.all(outputCSR.map((el) => bundleCSR.write(el)));
+
+  //SSR
+  const bundleSSR = await rollup.rollup(configSSR);
+  await Promise.all(outputSSR.map((el) => bundleSSR.write(el)));
+};
+
+build();
