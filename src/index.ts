739100be57b6f1b12db27b67c7a682af18e1600c
Timofey Goncharov <gtosss@mail.ru>
2021-01-13 23:36:20 +0300

    Add ts methods in index file.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import useFieldArray from './use-field-array';
 import useError from './use-error';
 import createFieldArray from './factories/create-field-array';
 import createForm from './factories/create-form';
-import { deleteIn, getIn, setIn, makeNested, removeFromInlineMap } from './utils/object-manager';
+import {
+  deleteIn,
+  getIn,
+  setIn,
+  makeNested,
+  removeFromInlineMap,
+  GetName,
+  getName,
+  GetNameStr,
+  getInTs,
+  getNameStr,
+} from './utils/object-manager';
 
+export * from './ts';
 export * from './ts';
 export {
   createForm,
@@ -17,4 +29,9 @@ export {
   setIn,
   makeNested,
   removeFromInlineMap,
+  GetName,
+  getName,
+  GetNameStr,
+  getInTs,
+  getNameStr,
 };
