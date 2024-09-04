8073a9ce2288554bd6aa995228ee0a5b1258fe4f
Denis Churbanov <churbanov.dv@gmail.com>
2024-09-04 17:12:54 +0400

    chore: update build config

--- tools/builder/rollup.js
+++ tools/builder/rollup.js
@@ -4,8 +4,6 @@ const { terser } = require('rollup-plugin-terser');
 const rollup = require('rollup');
 
 const input = 'src/index.ts';
-const externalCSR = ['lodash.topath', 'react', 'effector', 'effector-react', 'effector-react'];
-const externalSSR = ['lodash.topath', 'react', 'effector', 'effector-react', 'effector-react'];
 
 const configCSR = {
   input,
@@ -31,7 +29,7 @@ const configCSR = {
     }),
     // terser(),
   ],
-  external: externalCSR,
+  external: ['lodash.topath', 'react', 'effector', 'effector-react', 'effector-react'],
 };
 
 const outputCSR = [
@@ -45,60 +43,9 @@ const outputCSR = [
   },
 ];
 
-const configSSR = {
-  input,
-  plugins: [
-    nodeResolve({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
-    babel({
-      presets: [
-        [
-          '@babel/env',
-          {
-            loose: true,
-            modules: false,
-          },
-        ],
-        '@babel/preset-react',
-      ],
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.ts'],
-      plugins: [
-        ['@babel/plugin-transform-typescript'],
-        [
-          'effector/babel-plugin',
-          {
-            factories: ['src/factories/create-form', 'src/factories/create-field-array'],
-          },
-        ],
-      ],
-    }),
-    // terser(),
-  ],
-  external: externalSSR,
-};
-
-const outputSSR = [
-  {
-    file: `scope.js`,
-    format: 'cjs',
-  },
-  {
-    file: `scope.esm.js`,
-    format: 'esm',
-  },
-];
-
 const build = async () => {
-  // CSR
-  const bundleCSR = await rollup.rollup(configCSR);
-  await Promise.all(outputCSR.map((el) => bundleCSR.write(el)));
-
-  // SSR
-  const bundleSSR = await rollup.rollup(configSSR);
-  await Promise.all(outputSSR.map((el) => bundleSSR.write(el)));
+  const bundle = await rollup.rollup(configCSR);
+  await Promise.all(outputCSR.map((el) => bundle.write(el)));
 };
 
 build();
